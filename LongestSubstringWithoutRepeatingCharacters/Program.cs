aa63b5957b360fcffaf4f461f74b6145bb86e9f5
Vít Chvoj <vit.chvoj@verisoft.cz>
2023-12-03 01:37:27 +0100

    Added linq solution of the problem and used stopWatch to demonstrate speed differences

--- LongestSubstringWithoutRepeatingCharacters/Program.cs
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace MedianOfTwoSortedArrays
+﻿using System.Diagnostics;
+using System.Timers;
+
+namespace MedianOfTwoSortedArrays
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(FindMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }));
+            Stopwatch sw = new Stopwatch();
+            var nums1 = new int[] { 1, 3 };
+            var nums2 = new int[] { 2 };
+            sw.Start();
+            var linqResult = FindMedianSortedArraysLinqVersion(nums1, nums2);
+            sw.Stop();
+            Console.WriteLine($"Linq: {linqResult} | elapsed: {sw.Elapsed.TotalMilliseconds} ms");
+            sw.Restart();
+            var whileResult = FindMedianSortedArrays(nums1, nums2);
+            sw.Stop();
+            Console.WriteLine($"While: {whileResult} | elapsed: {sw.Elapsed.TotalMilliseconds} ms");
         }
 
         static double FindMedianSortedArrays(int[] nums1, int[] nums2)
@@ -28,6 +41,22 @@
             while (j < nums2.Length) mergedNumbers.Add(nums2[j++]);
 
             int mid = mergedNumbers.Count / 2;
+            if (mergedNumbers.Count > 0 && mergedNumbers.Count % 2 == 0)
+            {
+                return (mergedNumbers[mid] + mergedNumbers[mid - 1]) / 2.0;
+            }
+            else
+            {
+                return mergedNumbers[mid];
+            }
+        }
+
+        static double FindMedianSortedArraysLinqVersion(int[] nums1, int[] nums2)
+        {
+            var mergedNumbers = nums1.Concat(nums2).OrderBy(x => x).ToList();
+
+            int mid = mergedNumbers.Count / 2;
+
             if (mergedNumbers.Count > 0 && mergedNumbers.Count % 2 == 0)
             {
                 return (mergedNumbers[mid] + mergedNumbers[mid - 1]) / 2.0;
